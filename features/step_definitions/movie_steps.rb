3a9ba6743884a6f450a2ecad722299483ac3cf63
markus <mr.markus.kaiser@gmail.com>
2012-10-20 17:21:25 +0200

    hw3_part1 done - everything is green

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,13 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    #p movie
+    m=Movie.new(movie)
+    m.save!
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
